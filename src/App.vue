a1ef204a6014c684a1ce41fd15f9db86c27a911c
mguedesdev <mathheus.gr@gmail.com>
2024-01-03 09:58:07 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <RouterView />
+  </div>
+  
+</template>
+
+<script>
+
+import { RouterView } from 'vue-router'
+
+export default {
+  name: 'App',
+  components: {
+  },
+  setup() {
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "weather-primary": "#00668A",
+        "weather-secondary": "#004E71",
+      }
+    },
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+    }
+  },
+  plugins: [],
+}
+
